2a269c045888c3c412abe9c30ccd5a7a09937167
Yaoyu Chen <yaoyuchen@eml.cc>
2022-01-17 21:01:26 -0800

    Make OffCanvasMenu look the same as desktop's

--- src/lib/components/layout/OffCanvasMenu.svelte
+++ src/lib/components/layout/OffCanvasMenu.svelte
@@ -1,9 +1,18 @@
 <script>
 	import classes from 'svelte-transition-classes';
+	import { page } from '$app/stores';
+	import { menuItems } from '$lib/menu-items';
 	export let show = false;
+
+	import ReactivePanel from '$lib/components/layout/ReactivePanel.svelte';
+	import LoginForm from '$lib/components/feature/auth/LoginForm.svelte';
+	let showLogin = false;
 </script>
 
 {#if show}
+	<ReactivePanel bind:active={showLogin} title="Login">
+		<LoginForm />
+	</ReactivePanel>
 	<!-- Off-canvas menu for mobile, show/hide based on off-canvas menu state. -->
 	<div class="fixed inset-0 flex z-40 lg:hidden" role="dialog" aria-modal="true">
 		<!--
@@ -65,7 +74,7 @@
 				<button
 					on:click={() => (show = !show)}
 					type="button"
-					class="ml-1 flex items-center justify-center h-10 w-10 rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+					class="ml-1 flex items-center justify-center h-10 w-10 rounded-full focus:outline-none"
 				>
 					<span class="sr-only">Close sidebar</span>
 					<!-- Heroicon name: outline/x -->
@@ -89,179 +98,50 @@
 
 			<div class="flex-1 h-0 pt-5 pb-4 overflow-y-auto">
 				<div class="flex-shrink-0 flex items-center px-4">
-					<img
-						class="h-8 w-auto"
-						src="https://tailwindui.com/img/logos/workflow-logo-indigo-600-mark-gray-900-text.svg"
-						alt="Workflow"
-					/>
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"
+						><path fill="none" d="M0 0h24v24H0z" /><path
+							d="M3 3h18a1 1 0 0 1 1 1v16a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1zm9 12v2h6v-2h-6zm-3.586-3l-2.828 2.828L7 16.243 11.243 12 7 7.757 5.586 9.172 8.414 12z"
+						/></svg
+					>
 				</div>
 				<nav aria-label="Sidebar" class="mt-5">
-					<div class="px-2 space-y-1">
-						<!-- Current: "bg-gray-100 text-gray-900", Default: "text-gray-600 hover:bg-gray-50 hover:text-gray-900" -->
-						<a
-							href="#"
-							class="bg-gray-100 text-gray-900 group flex items-center px-2 py-2 text-base font-medium rounded-md"
-						>
-							<!--
-                Heroicon name: outline/home
-
-                Current: "text-gray-500", Default: "text-gray-400 group-hover:text-gray-500"
-              -->
-							<svg
-								class="text-gray-500 mr-4 h-6 w-6"
-								xmlns="http://www.w3.org/2000/svg"
-								fill="none"
-								viewBox="0 0 24 24"
-								stroke="currentColor"
-								aria-hidden="true"
-							>
-								<path
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									stroke-width="2"
-									d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-								/>
-							</svg>
-							Articles
-						</a>
-
-						<a
-							href="#"
-							class="text-gray-600 hover:bg-gray-50 hover:text-gray-900 group flex items-center px-2 py-2 text-base font-medium rounded-md"
-						>
-							<!-- Heroicon name: outline/calendar -->
-							<svg
-								class="text-gray-400 group-hover:text-gray-500 mr-4 h-6 w-6"
-								xmlns="http://www.w3.org/2000/svg"
-								fill="none"
-								viewBox="0 0 24 24"
-								stroke="currentColor"
-								aria-hidden="true"
-							>
-								<path
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									stroke-width="2"
-									d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
-								/>
-							</svg>
-							Calendar
-						</a>
-
-						<a
-							href="#"
-							class="text-gray-600 hover:bg-gray-50 hover:text-gray-900 group flex items-center px-2 py-2 text-base font-medium rounded-md"
-						>
-							<!-- Heroicon name: outline/user-group -->
-							<svg
-								class="text-gray-400 group-hover:text-gray-500 mr-4 h-6 w-6"
-								xmlns="http://www.w3.org/2000/svg"
-								fill="none"
-								viewBox="0 0 24 24"
-								stroke="currentColor"
-								aria-hidden="true"
-							>
-								<path
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									stroke-width="2"
-									d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"
-								/>
-							</svg>
-							Teams
-						</a>
-
-						<a
-							href="#"
-							class="text-gray-600 hover:bg-gray-50 hover:text-gray-900 group flex items-center px-2 py-2 text-base font-medium rounded-md"
-						>
-							<!-- Heroicon name: outline/search-circle -->
-							<svg
-								class="text-gray-400 group-hover:text-gray-500 mr-4 h-6 w-6"
-								xmlns="http://www.w3.org/2000/svg"
-								fill="none"
-								viewBox="0 0 24 24"
-								stroke="currentColor"
-								aria-hidden="true"
-							>
-								<path
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									stroke-width="2"
-									d="M8 16l2.879-2.879m0 0a3 3 0 104.243-4.242 3 3 0 00-4.243 4.242zM21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-								/>
-							</svg>
-							Directory
-						</a>
-
-						<a
-							href="#"
-							class="text-gray-600 hover:bg-gray-50 hover:text-gray-900 group flex items-center px-2 py-2 text-base font-medium rounded-md"
-						>
-							<!-- Heroicon name: outline/speakerphone -->
-							<svg
-								class="text-gray-400 group-hover:text-gray-500 mr-4 h-6 w-6"
-								xmlns="http://www.w3.org/2000/svg"
-								fill="none"
-								viewBox="0 0 24 24"
-								stroke="currentColor"
-								aria-hidden="true"
-							>
-								<path
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									stroke-width="2"
-									d="M11 5.882V19.24a1.76 1.76 0 01-3.417.592l-2.147-6.15M18 13a3 3 0 100-6M5.436 13.683A4.001 4.001 0 017 6h1.832c4.1 0 7.625-1.234 9.168-3v14c-1.543-1.766-5.067-3-9.168-3H7a3.988 3.988 0 01-1.564-.317z"
-								/>
-							</svg>
-							Announcements
-						</a>
-
-						<a
-							href="#"
-							class="text-gray-600 hover:bg-gray-50 hover:text-gray-900 group flex items-center px-2 py-2 text-base font-medium rounded-md"
-						>
-							<!-- Heroicon name: outline/map -->
-							<svg
-								class="text-gray-400 group-hover:text-gray-500 mr-4 h-6 w-6"
-								xmlns="http://www.w3.org/2000/svg"
-								fill="none"
-								viewBox="0 0 24 24"
-								stroke="currentColor"
-								aria-hidden="true"
-							>
-								<path
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									stroke-width="2"
-									d="M9 20l-5.447-2.724A1 1 0 013 16.382V5.618a1 1 0 011.447-.894L9 7m0 13l6-3m-6 3V7m6 10l4.553 2.276A1 1 0 0021 18.382V7.618a1 1 0 00-.553-.894L15 4m0 13V4m0 0L9 7"
-								/>
-							</svg>
-							Office Map
-						</a>
+					<div class="px-2">
+						{#each menuItems as group}
+							<div class="pt-3 first:pt-0">
+								{#if group.name}
+									<div
+										class="text-gray-900 flex items-center px-2 py-2 font-medium text-sm rounded-md"
+									>
+										{group.name}
+									</div>
+								{/if}
+								{#each group.items as item}
+									<a
+										href={item.href}
+										target={item.href.startsWith('http') ? '_blank' : ''}
+										class="{$page.url.pathname === item.href
+											? 'text-gray-900 bg-gray-50'
+											: 'text-gray-700 hover:bg-gray-50 hover:text-gray-900 transition-colors'} flex items-center px-2 py-2 mb-1 text-sm rounded-md"
+									>
+										<i class="{item.iconClass} mr-2 items-center" />
+										{item.label}
+										{#if item.href.startsWith('http')}
+											<i class="ri-arrow-right-up-line ml-auto text-gray-400" />
+										{/if}
+									</a>
+								{/each}
+							</div>
+						{/each}
 					</div>
 				</nav>
 			</div>
 			<div class="flex-shrink-0 flex border-t border-gray-200 p-4">
-				<a href="#" class="flex-shrink-0 group block">
-					<div class="flex items-center">
-						<div>
-							<img
-								class="inline-block h-10 w-10 rounded-full"
-								src="https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80"
-								alt=""
-							/>
-						</div>
-						<div class="ml-3">
-							<p class="text-base font-medium text-gray-700 group-hover:text-gray-900">
-								Whitney Francis
-							</p>
-							<p class="text-sm font-medium text-gray-500 group-hover:text-gray-700">
-								View profile
-							</p>
-						</div>
-					</div>
-				</a>
+				<button
+					on:click={() => (showLogin = !showLogin)}
+					class="flex-shrink-0 w-full group block text-center text-sm text-gray-500"
+				>
+					Login
+				</button>
 			</div>
 		</div>
 
