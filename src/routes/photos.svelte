f89503a2c2460698fc32ab9bcbd748a85bfdcb96
Runkai Zhang <me@runkaizhang.xyz>
2022-01-26 09:45:11 -0500

    Fix a warning

--- src/routes/photos.svelte
+++ src/routes/photos.svelte
@@ -34,7 +34,7 @@
 
           <!-- Gallery -->
           <section class="mt-8 pb-16" aria-labelledby="gallery-heading">
-            <ul role="list" class="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 md:grid-cols-4 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+            <ul class="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 md:grid-cols-4 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
                 {#await promise then value}
                 {#each value as photo}
                     <button on:click={() => selectPhoto(photo)}><Photo photo={photo} /></button>
