5097c126932e3b2eda2d5c867a05e5a22e303285
Yaoyu Chen <yaoyuchen@eml.cc>
2022-01-25 18:19:34 -0800

    Add a placeholder for hooks.js

---
+++ src/hooks.js
@@ -0,0 +1,4 @@
+// Note: We are NOT using hooks for user authentication at this point.
+//       Using hooks for auth means we will have to check for auth every
+//       time a logged in user accesses a page, which is a potential
+//       performance issue.
