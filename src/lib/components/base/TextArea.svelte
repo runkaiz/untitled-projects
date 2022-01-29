fa2b3bf492c4e2446a420fe8717d3c022b5f18a8
Yaoyu Chen <yaoyuchen@eml.cc>
2022-01-29 14:12:35 -0800

    Implement `TextArea`

--- src/lib/components/base/TextArea.svelte
+++ src/lib/components/base/TextArea.svelte
@@ -1,8 +1,25 @@
 <script>
-	export let tagline;
-	export let placeholder;
-	export let value;
-	export let rows;
+	export let label;
+	export let name;
+	export let placeholder = '';
+	export let value = '';
+	export let disabled = false;
+	export let required = false;
 </script>
 
-Not implemented yet.
+<div>
+	<label for={name} class="block text-sm font-medium text-gray-700">
+		{label}
+	</label>
+	<div class="mt-1 relative">
+		<textarea
+			{name}
+			id={name}
+			{placeholder}
+			bind:value
+			{disabled}
+			{required}
+			class="appearance-none block w-full py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:shadow-md transition-shadow sm:text-sm"
+		/>
+	</div>
+</div>
