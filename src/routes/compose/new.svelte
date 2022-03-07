425a6f6c0814114cfab38bc8c5f4912731bf8d7a
Yaoyu Chen <yaoyuchen@eml.cc>
2022-03-07 21:39:11 +0100

    Protect composer with permission check

--- src/routes/compose/new.svelte
+++ src/routes/compose/new.svelte
@@ -1,3 +1,14 @@
+<script context="module">
+	export async function load({ session, fetch }) {
+		if (!session.user) {
+			return {
+				status: 307,
+				redirect: '/'
+			};
+		}
+	}
+</script>
+
 <script>
 	import Composer from '$lib/components/feature/Composer.svelte';
 	import MainPanel from '$lib/components/layout/MainPanel.svelte';
