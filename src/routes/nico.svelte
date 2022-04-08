ccd3a49353d111c2b577e6bdb74ebedfd5e99cda
Runkai Zhang <me@runkaizhang.xyz>
2022-04-08 14:10:31 +0000

    Add a template for my personal page
    And also just to test out GitPod on an iPad.

---
+++ src/routes/nico.svelte
@@ -0,0 +1,18 @@
+<script>
+	import MainPanel from '$lib/components/layout/MainPanel.svelte';
+</script>
+
+<MainPanel>
+	<h1 class="font-semibold text-2xl">Runkai (Nico) Zhang</h1>
+	<p class="pt-2 text-gray-700">
+		I am an high school student pursuing my interests in computer science.
+	</p>
+	<div class="flex flex-row">
+		<div class="basis-full border border-gray-200 rounded-md p-4 my-6">
+			<h2 class="font-semibold text-lg">A work in progress.</h2>
+			<p class="pt-2 text-gray-700">
+                I am still compiling and designing what is going to be placed inside this section, so bear with me for now.
+			</p>
+		</div>
+	</div>
+</MainPanel>
