d93abd27f66be032ff96aefa28222269be32e7db
Runkai Zhang <me@runkaizhang.xyz>
2022-04-18 16:52:20 -0400

    Fix the annoying save button that does nothing in the Meta panel

--- src/lib/components/feature/Composer.svelte
+++ src/lib/components/feature/Composer.svelte
@@ -53,6 +53,10 @@
 			alert('Error saving note.');
 		}
 	}
+
+	function closePanel() {
+		showMeta = false;
+	}
 </script>
 
 <div class="flex flex-col h-full">
@@ -154,7 +158,7 @@
 
 		<div>
 			<button
-				class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+				on:click="{closePanel}" class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
 			>
 				Save
 			</button>
