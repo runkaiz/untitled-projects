6a98154713f634428c8b15f4b2c790dd9039c1a7
Runkai Zhang <me@runkaizhang.xyz>
2022-05-21 11:49:32 -0400

    Initialize eco mode

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,6 +4,15 @@
 
 <MainPanel>
 	<h1 class="font-semibold text-2xl">Untitled Projects</h1>
+	<div class="flex flex-row">
+		<div class="basis-full border border-gray-200 rounded-md p-4 my-6">
+			<h2 class="font-semibold text-lg"><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}><path strokeLinecap="round" strokeLinejoin="round" d="M13 10V3L4 14h7v7l9-11h-7z" /></svg>
+			</h2>
+			<p class="pt-2 text-gray-700">
+				You have discovered this website in eco mode, which means the current web page you are viewing is hosted on a solar powered SBC. This helps us reduce load from our primary webserver and take another step towards carbon neutrality.
+			</p>
+		</div>
+	</div>
 	<p class="pt-2 text-gray-700">
 		This is a collaborative space for nico and yao to document their shared projects.
 	</p>
