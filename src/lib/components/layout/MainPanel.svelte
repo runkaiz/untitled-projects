58b184b6061edad6df60a62751d109548265fd99
Runkai Zhang <me@runkaizhang.xyz>
2022-06-07 12:04:38 -0400

    Fix a merge error

--- src/lib/components/layout/MainPanel.svelte
+++ src/lib/components/layout/MainPanel.svelte
@@ -1,15 +1,5 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<main class="flex-1 relative z-0 overflow-y-auto focus:outline-none xl:order-last">
-	<div class="max-w-4xl mx-auto px-4 pt-16 pb-3 lg:px-8">
-=======
 <main class="flex-1 relative z-0 overflow-y-auto focus:outline-none order-last">
 	<div class="max-w-4xl mx-auto px-4 pt-10 pb-3 sm:px-8">
->>>>>>> 0149733 (Improve mobile support)
-=======
-<main class="flex-1 relative z-0 overflow-y-auto focus:outline-none order-last">
-	<div class="max-w-4xl mx-auto px-4 pt-10 pb-3 sm:px-8">
->>>>>>> 4a66f8089edf46a8ea69a2fc8e6170aa0616144b
 		<slot />
 	</div>
 </main>
