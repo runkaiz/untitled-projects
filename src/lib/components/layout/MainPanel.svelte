584b48a4f3daff12b3a9d8067d8ebb9c33b4de34
Yaoyu Chen <yaoyuchen@eml.cc>
2022-06-07 20:05:09 +0545

    Adjust mobile layout

--- src/lib/components/layout/MainPanel.svelte
+++ src/lib/components/layout/MainPanel.svelte
@@ -1,5 +1,5 @@
 <main class="flex-1 relative z-0 overflow-y-auto focus:outline-none xl:order-last">
-	<div class="max-w-4xl mx-auto px-4 pt-10 pb-3 lg:px-8">
+	<div class="max-w-4xl mx-auto px-4 pt-16 pb-3 lg:px-8">
 		<slot />
 	</div>
 </main>
