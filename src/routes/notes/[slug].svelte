1a3151df689f88729f304e0eb59807340e983d7b
Yaoyu Chen <yaoyuchen@eml.cc>
2022-06-09 13:02:35 +0545

    I was tripping

--- src/routes/notes/[slug].svelte
+++ src/routes/notes/[slug].svelte
@@ -45,43 +45,45 @@
 	}
 </script>
 
-{#if note.isDraft && $session.user && $session.user.isAdmin}
-	<!-- This example requires Tailwind CSS v2.0+ -->
-	<div class="bg-gray-50 rounded-md sm:rounded-lg mb-6">
-		<div class="px-4 py-5 sm:p-6">
-			<h3 class="text-lg leading-6 font-medium text-gray-900">This is a draft.</h3>
-			<div class="mt-2 max-w-xl text-sm text-gray-500">
-				<p>Visitors cannot see this post just yet. Enter edit mode to publish.</p>
-			</div>
-			<div class="mt-5">
-				<button
-					type="button"
-					class="inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
-					on:click={() => {
-						goto(`/compose/${note.slug}`);
-					}}>Open Composer</button
-				>
-			</div>
-		</div>
-	</div>
-{:else}
-	<div class="bg-gray-50 rounded-md sm:rounded-lg mb-6">
-		<div class="px-4 py-5 sm:p-6">
-			<h3 class="text-lg leading-6 font-medium text-gray-900">Published!</h3>
-			<div class="mt-2 max-w-xl text-sm text-gray-500">
-				<p>This note had already been published</p>
-			</div>
-			<div class="mt-5">
-				<button
-					type="button"
-					class="inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
-					on:click={() => {
-						goto(`/compose/${note.slug}`);
-					}}>Open Composer</button
-				>
+{#if $session.user && $session.user.isAdmin}
+	{#if note.isDraft}
+		<!-- This example requires Tailwind CSS v2.0+ -->
+		<div class="bg-gray-50 rounded-md sm:rounded-lg mb-6">
+			<div class="px-4 py-5 sm:p-6">
+				<h3 class="text-lg leading-6 font-medium text-gray-900">This is a draft.</h3>
+				<div class="mt-2 max-w-xl text-sm text-gray-500">
+					<p>Visitors cannot see this post just yet. Enter edit mode to publish.</p>
+				</div>
+				<div class="mt-5">
+					<button
+						type="button"
+						class="inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
+						on:click={() => {
+							goto(`/compose/${note.slug}`);
+						}}>Open Composer</button
+					>
+				</div>
 			</div>
 		</div>
-	</div>
+	{:else}
+		<div class="bg-gray-50 rounded-md sm:rounded-lg mb-6">
+			<div class="px-4 py-5 sm:p-6">
+				<h3 class="text-lg leading-6 font-medium text-gray-900">Published!</h3>
+				<div class="mt-2 max-w-xl text-sm text-gray-500">
+					<p>This note had already been published</p>
+				</div>
+				<div class="mt-5">
+					<button
+						type="button"
+						class="inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
+						on:click={() => {
+							goto(`/compose/${note.slug}`);
+						}}>Open Composer</button
+					>
+				</div>
+			</div>
+		</div>
+	{/if}
 {/if}
 
 <h1 class="text-2xl font-bold">{note.title}</h1>
