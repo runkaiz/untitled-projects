10613946dbbe625fa5598a1c05dbf047464d0a15
Runkai Zhang <me@runkaizhang.xyz>
2022-06-12 19:32:11 -0400

    Clean up a todo from last commit.

--- src/lib/components/feature/auth/LoginForm.svelte
+++ src/lib/components/feature/auth/LoginForm.svelte
@@ -44,7 +44,6 @@
 				type="checkbox"
 				class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
 			/>
-			<!-- TODO: Handle this... -->
 			<label for="remember-me" class="ml-2 block text-sm text-gray-900"> Remember me </label>
 		</div>
 
