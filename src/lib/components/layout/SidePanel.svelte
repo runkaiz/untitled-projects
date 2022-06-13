5b343ab1015d64b14201699496817e0e095e2277
Yaoyu Chen <yaoyuchen@eml.cc>
2022-06-13 09:37:37 +0545

    Adjust layout
    - Decreased `max-w` for the `MainPanel`
    - Decreased the width of `SidePanel`

--- src/lib/components/layout/SidePanel.svelte
+++ src/lib/components/layout/SidePanel.svelte
@@ -1,5 +1,5 @@
 <div
-	class="divide-y relative order-3 flex flex-col flex-shrink-0 max-w-full max-h-screen sm:w-96 border-r border-gray-200 overflow-y-scroll"
+	class="divide-y relative order-3 flex flex-col flex-shrink-0 max-w-full max-h-screen sm:w-80 border-r border-gray-200 overflow-y-scroll"
 >
 	<slot />
 </div>
