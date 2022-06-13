4d9e7b33c5f5001f3be08e4b5192832c958bd7f0
Runkai Zhang <me@runkaizhang.xyz>
2022-06-13 16:37:32 -0400

    Fix a regression where the save button doesn't close the meta panel

--- src/lib/components/feature/Composer.svelte
+++ src/lib/components/feature/Composer.svelte
@@ -285,7 +285,7 @@
 		<div>
 			<button
 				on:click={() => {
-					showMeta = true;
+					showMeta = false;
 				}}
 				class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
 			>
