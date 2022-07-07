66642583228486fc152f9190f7b7abe63f1bf95a
Runkai Zhang <me@runkaizhang.xyz>
2022-07-07 15:27:55 -0400

    SvelteKit had a weird update regarding vite, from now on we are going to use vite itself instead of the sveltekit wrapper, thus allowing better integration with tools like storybook (which I recommand trying out)

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,15 @@
+import adapter from '@sveltejs/adapter-auto';
 import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
-import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: ['.svelte', ...mdsvexConfig.extensions],
 	kit: {
 		adapter: adapter()
 	},
+    extensions: ['.svelte', ...mdsvexConfig.extensions],
 
-	preprocess: [mdsvex(mdsvexConfig)]
+    preprocess: [mdsvex(mdsvexConfig)]
 };
 
 export default config;
