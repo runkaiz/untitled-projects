3c93849ade98f383979694847293a1e547721770
Runkai Zhang <me@runkaizhang.xyz>
2022-08-04 14:29:10 -0400

    Add a placeholder page for yao to avoid the 404

---
+++ src/routes/yao.svelte
@@ -0,0 +1,17 @@
+<script>
+	import DescriptionBox from '$lib/components/content/DescriptionBox.svelte';
+	import HyperLink from '$lib/components/content/HyperLink.svelte';
+	import MainPanel from '$lib/components/layout/MainPanel.svelte';
+	import { site_title } from '$lib/environment';
+</script>
+
+<svelte:head>
+	<title>{site_title} • Yao</title>
+</svelte:head>
+
+<MainPanel>
+	<h1>Yaoyu Chen</h1>
+	<p class="pt-2">
+		This is a placeholder.
+	</p>
+</MainPanel>
